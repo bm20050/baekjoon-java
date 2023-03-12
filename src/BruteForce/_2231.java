999dd7a54c0c840905d3ae9f7156c3e52d08c562
bm20050 <bm20050@gmail.com>
2023-03-12 22:51:10 +0900

    03.12 update

---
+++ src/BruteForce/_2231.java
@@ -0,0 +1,23 @@
+package BruteForce;
+
+import java.util.Scanner;
+
+public class _2231 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int x = i;
+            int sum = 0;
+            while (x > 0) {
+                sum += x % 10;
+                x = x / 10;
+            }
+            if (i + sum == n) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
